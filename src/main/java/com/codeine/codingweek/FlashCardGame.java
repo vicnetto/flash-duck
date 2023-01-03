7f40d8bbd8130b537be214aee5b605273c160c58
Theo <theo.fontenit@telecomnancy.eu>
2023-01-03 14:04:09 +0100

    Méthodes manipulation lesPiles FlashCardGame

--- src/main/java/com/codeine/codingweek/FlashCardGame.java
+++ src/main/java/com/codeine/codingweek/FlashCardGame.java
@@ -1,4 +1,34 @@
 package com.codeine.codingweek;
 
+import java.util.ArrayList;
+
 public class FlashCardGame {
+
+    private ArrayList<Pile> lesPiles;
+
+    public FlashCardGame() {
+    }
+
+    public ArrayList<Pile> getLesPiles() {
+        return lesPiles;
+    }
+
+    public void setLesPiles(ArrayList<Pile> lesPiles) {
+        this.lesPiles = lesPiles;
+    }
+
+    public void addPile(Pile laPile) {
+        this.lesPiles.add(laPile);
+    }
+
+    public void removePileByNom(String nomPile) {
+        int i = 0;
+        while (i < lesPiles.size()) {
+            if (lesPiles.get(i).getName().equals(nomPile)) {
+                lesPiles.remove(i);
+                return;
+            }
+            i++;
+        }
+    }
 }
