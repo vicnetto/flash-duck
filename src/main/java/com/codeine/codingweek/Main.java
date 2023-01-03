42328ed3c5f3a1d67d1932948867248ff73f9c7d
Chevallier Arthur <arthur.chevallier@telecomnancy.eu>
2023-01-03 14:59:58 +0100

    CSS fonctionnel

--- src/main/java/com/codeine/codingweek/Main.java
+++ src/main/java/com/codeine/codingweek/Main.java
@@ -12,13 +12,17 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        Scene scene = new Scene(new BorderPane());
-        ViewSwitcher.setScene(scene);
-        ViewSwitcher.swtichTo(View.ACCUEIL);
+        Scene scene = new Scene(new BorderPane()) ;
+        
+        ViewSwitcher.setScene(scene) ;
+        ViewSwitcher.swtichTo(View.ACCUEIL) ;
 
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        scene.getStylesheets().add(getClass().getResource("css/style.css").toExternalForm()) ;
+
+        stage.setTitle("TN FlashCards") ;
+        stage.setScene(scene) ;
+        stage.show() ;
+
     }
 
     public static void main(String[] args) {
