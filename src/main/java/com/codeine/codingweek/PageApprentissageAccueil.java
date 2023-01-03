ba5f562d6456a232ffce1cdf666eb23d1ebe837e
edgarlaugeais <edgar.laugeais6@etu.univ-lorraine>
2023-01-03 17:01:53 +0100

    ajout controlleur page-apprentissage

---
+++ src/main/java/com/codeine/codingweek/PageApprentissageAccueil.java
@@ -0,0 +1,13 @@
+package com.codeine.codingweek;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PageApprentissageAccueil implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+}
--- src/main/java/com/codeine/codingweek/ViewSwitcher.java
+++ src/main/java/com/codeine/codingweek/ViewSwitcher.java
@@ -25,6 +25,7 @@ public class ViewSwitcher {
         loader.setLocation(ViewSwitcher.class.getResource(view.getFileName()));
 
         AccueilController accueilController = new AccueilController();
+
         CreationPileController creationPileController = new CreationPileController();
         CreationCarteController creationCarteController = new CreationCarteController();
         MenuController menuController = new MenuController();
@@ -34,6 +35,7 @@ public class ViewSwitcher {
             else if (ic.equals(CreationPileController.class)) return creationPileController;
             else if (ic.equals(CreationCarteController.class)) return creationCarteController;
             else if (ic.equals(MenuController.class)) return menuController;
+
             else System.out.println("Pas de controller trouve");
             return null;
         });
