deb1657c89899a76b4d9b988fd4434224c629af4
Matéo DEMANGEON <mateo.demangeon@telecomnancy.eu>
2023-01-04 15:11:44 +0100

    Catégories générales + bug thème sur Linux

--- src/main/java/com/codeine/codingweek/CreationPileController.java
+++ src/main/java/com/codeine/codingweek/CreationPileController.java
@@ -6,12 +6,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -36,7 +33,7 @@ public class CreationPileController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        textarea_c.setOnKeyReleased(event -> {
+        textarea_c.setOnKeyPressed(event -> {
             if (event.getCode() == KeyCode.TAB) {
                 event.consume();
 
@@ -48,6 +45,8 @@ public class CreationPileController implements Initializable {
                 for (String option : getAutocompleteOptions()) {
                     if (option.startsWith(text)) {
                         textarea_c.insertText(caretPosition - 1, option.substring(text.length()));
+                        textarea_c.setText(textarea_c.getText().replaceAll("\t", ""));
+                        textarea_c.positionCaret(textarea_c.getLength());
                         break;
                     }
                 }
@@ -58,7 +57,7 @@ public class CreationPileController implements Initializable {
 
     private List<String> getAutocompleteOptions() {
         // Termes possibles de l'auto-complétion
-        return Arrays.asList("Histoire", "Art", "Musique", "Cinéma", "Gastronomie", "Sport", "Science", "Littérature");
+        return fcg.getCategories();
     }
 
     public void annuler(ActionEvent actionEvent) throws  IOException {
@@ -67,7 +66,7 @@ public class CreationPileController implements Initializable {
 
     public void ajouterPile(ActionEvent actionEvent) throws IOException {
         String titre = this.textarea_t.getText();
-        String categorie = this.textarea_c.getText();
+        String categorie = this.textarea_c.getText().replaceAll("\t", "");
         this.fcg.addPile(new Pile(categorie, titre));
         ViewSwitcher.swtichTo(View.PILE_CREATION);
     }
