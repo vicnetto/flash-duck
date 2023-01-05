c5995a239a01434dfc8a93be8551eb0aa753db7c
Theo <theo.fontenit@telecomnancy.eu>
2023-01-05 15:05:19 +0100

    cancel lancer partie pile vide

--- src/main/java/com/codeine/codingweek/AfficherPiles.java
+++ src/main/java/com/codeine/codingweek/AfficherPiles.java
@@ -14,6 +14,7 @@ public class AfficherPiles {
     public static final int maxByLine = 4;
 
     public static final String cellStyle = "-fx-border-color: black;-fx-border-radius: 5; -fx-margin-bottom: 20; -fx-background-color: #f3f6dd;";
+    public static final String disableCellStyle = "-fx-border-color: black;-fx-border-radius: 5; -fx-margin-bottom: 20; -fx-background-color: #b4b4b4;";
 
     public static void afficherToutesLesPiles(FlashCardGame fcg, GridPane gridPiles, IntConsumer goToPile) {
         int line;
@@ -41,6 +42,20 @@ public class AfficherPiles {
             column = column == maxByLine - 1 ? 0 : column + 1;
 
             gridPiles.add(vb, column, line);
+        }
+    }
+
+    public static void griserPilesVides(FlashCardGame fcg, GridPane gridPiles) {
+        for (int i = 0; i < fcg.getLesPiles().size(); i++) {
+            VBox vb = (VBox) gridPiles.getChildren().get(i);
+            if (fcg.getLesPiles().get(i).getCards().size() == 0) {
+                vb.setStyle(disableCellStyle);
+                vb.setOnMouseEntered(action ->
+                        vb.setStyle(cellStyle + "-fx-background-color: #b4b4b4"));
+                vb.setOnMouseExited(action ->
+                        vb.setStyle(cellStyle + "-fx-background-color: #b4b4b4"));
+                vb.setOnMouseClicked(e -> {});
+            }
         }
     }
 }
