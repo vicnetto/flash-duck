ccc62c41b8b3cbbe1846e489a95ba31bbda32639
Theo <theo.fontenit@telecomnancy.eu>
2023-01-05 14:27:33 +0100

    compteur

--- src/main/java/com/codeine/codingweek/PageApprentissageWhatisaskedController.java
+++ src/main/java/com/codeine/codingweek/PageApprentissageWhatisaskedController.java
@@ -2,11 +2,17 @@ package com.codeine.codingweek;
 
 import com.codeine.codingweek.PatternStrategyQuestions.ApprentissageMethod;
 import com.codeine.codingweek.model.FlashCardGame;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.util.Duration;
 
 import java.io.IOException;
 import java.net.URL;
@@ -16,19 +22,47 @@ public class PageApprentissageWhatisaskedController implements Initializable {
     public Button asked;
     public Text hint;
     public VBox laVb;
+    public Label compteurLabel;
     private FlashCardGame fcg;
+    private int temps;
+    private Timeline tm;
 
     public PageApprentissageWhatisaskedController(FlashCardGame fcg) {
         this.fcg = fcg;
     }
 
+    public void compteurTemps() {
+        tm = new Timeline();
+        tm.setCycleCount(Timeline.INDEFINITE);
+        temps = 10;
+        this.compteurLabel.setText(String.valueOf(temps));
+        KeyFrame kf = new KeyFrame(Duration.seconds(1), new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent event) {
+                temps--;
+                compteurLabel.setText(String.valueOf(temps));
+                if (temps<=0){
+                    tm.stop();
+                    try {
+                        ViewSwitcher.switchTo(View.APPRENTISSAGE_CREATOR_REPONSE);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        });
+        tm.getKeyFrames().add(kf);
+        tm.playFromStart();
+    }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         ApprentissageMethod am = this.fcg.getCurrentApprentissageList().get(this.fcg.getCurrentIndexApprentissageList());
         am.afficher(this.laVb);
+        this.compteurTemps();
     }
 
     public void goToReponse(ActionEvent actionEvent) throws IOException {
+        tm.stop();
         ViewSwitcher.switchTo(View.APPRENTISSAGE_CREATOR_REPONSE);
     }
 }
