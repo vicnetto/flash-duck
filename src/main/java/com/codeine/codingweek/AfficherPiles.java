3b323a81d6bf1edf4c778077f6cc65bcb9dbe128
Victor Netto <victorhmnetto@gmail.com>
2023-01-05 23:29:36 +0100

    Removing cellStyle constant and adding a css rule

--- src/main/java/com/codeine/codingweek/AfficherPiles.java
+++ src/main/java/com/codeine/codingweek/AfficherPiles.java
@@ -11,10 +11,7 @@ import java.util.function.IntConsumer;
 
 public class AfficherPiles {
 
-    public static final int maxByLine = 4;
-
-    public static final String cellStyle = "-fx-border-color: black;-fx-border-radius: 5; -fx-margin-bottom: 20; -fx-background-color: #f3f6dd;";
-    public static final String disableCellStyle = "-fx-border-color: black;-fx-border-radius: 5; -fx-margin-bottom: 20; -fx-background-color: #b4b4b4;";
+    public static final int MAX_BY_LINE = 4;
 
     public static void afficherToutesLesPiles(FlashCardGame fcg, GridPane gridPiles, IntConsumer goToPile) {
         int line;
@@ -25,11 +22,7 @@ public class AfficherPiles {
             vb.setPrefSize(150, 80);
             vb.setAlignment(Pos.CENTER);
             vb.setPadding(new Insets(10));
-            vb.setStyle(cellStyle);
-            vb.setOnMouseEntered(action ->
-                    vb.setStyle(cellStyle + "-fx-background-color: #ffd064"));
-            vb.setOnMouseExited(action ->
-                    vb.setStyle(cellStyle + "-fx-background-color: #f3f6dd"));
+            vb.getStyleClass().add("pile_show");
 
             Label nom = new Label(fcg.getLesPiles().get(i).getName());
             nom.setStyle("-fx-text-fill: #000000");
@@ -38,8 +31,8 @@ public class AfficherPiles {
             int ii = i;
             vb.setOnMouseClicked(e -> goToPile.accept(ii));
 
-            line = i / maxByLine;
-            column = column == maxByLine - 1 ? 0 : column + 1;
+            line = i / MAX_BY_LINE;
+            column = column == MAX_BY_LINE - 1 ? 0 : column + 1;
 
             gridPiles.add(vb, column, line);
         }
@@ -48,12 +41,8 @@ public class AfficherPiles {
     public static void griserPilesVides(FlashCardGame fcg, GridPane gridPiles) {
         for (int i = 0; i < fcg.getLesPiles().size(); i++) {
             VBox vb = (VBox) gridPiles.getChildren().get(i);
-            if (fcg.getLesPiles().get(i).getCards().size() == 0) {
-                vb.setStyle(disableCellStyle);
-                vb.setOnMouseEntered(action ->
-                        vb.setStyle(cellStyle + "-fx-background-color: #b4b4b4"));
-                vb.setOnMouseExited(action ->
-                        vb.setStyle(cellStyle + "-fx-background-color: #b4b4b4"));
+            if (fcg.getLesPiles().get(i).getCards().isEmpty()) {
+                vb.getStyleClass().add("pile_show_grey");
                 vb.setOnMouseClicked(e -> {});
             }
         }
